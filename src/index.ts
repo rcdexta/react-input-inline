e4ee10590b31f364612d270f227ce0db9b6170ef
RC <rc.chandru@gmail.com>
2021-06-01 22:55:02 -0700

    Expose SelectField as named import

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import NumberField from './NumberField/NumberField'
 import TextField from './TextField/TextField'
+import SelectField from './SelectField/SelectField'
 
 
-export { TextField, NumberField };
+export { TextField, NumberField, SelectField };
